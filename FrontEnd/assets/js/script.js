57a9bc8a5ada7ec598e81d7c093a54a4e1f1bb21
Pierre-Alexandre Augustin <alexandre.augustin10@gmail.com>
2024-05-16 14:30:56 +0200

    Intégration de la partie filtres depuis l'API

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,9 +1,11 @@
+/**************** WORKS  *****************/
+
 // Récupération de la class présent dans le HTML
-const gallery = document.querySelector(".gallery"); // Utiliser querySelector et non querySelectorAll
+let gallery = document.querySelector(".portfolio__gallery"); // Utiliser querySelector et non querySelectorAll
 
 // Création de la function Json
 async function getWorks() {
-    const response = await fetch("http://localhost:5678/api/works"); // Récupération obets avec la méthode fetch
+    let response = await fetch("http://localhost:5678/api/works"); // Récupération obets avec la méthode fetch
     // const responseJson = await response.json(); // Convertit la réponse en json
     // console.log(responseJson);  // Contrôle le retour des objet dans la console
     return await response.json(); // Retourne la réponse en json
@@ -13,13 +15,13 @@ getWorks();
 // Création function affichage
 async function displayWorks() {
     // Création variable pour récupérer la response.json
-    const listWorks = await getWorks();
+    let listWorks = await getWorks();
     // Création de la boucle pour parcourir les objets
     listWorks.forEach((works) => {
         // Création des balises sémantiques dans le DOM
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let figcaption = document.createElement("figcaption");
         // Mise en place de la hiérarchie Parent/Enfant
         gallery.appendChild(figure);
         figure.appendChild(img);
@@ -32,3 +34,49 @@ async function displayWorks() {
 
 //Appel de la fonction
 displayWorks();
+
+/************** FILTRES CATEGORIES ************/
+
+let filters = document.querySelector(".portfolio__filters");
+let all = "Tous";
+
+async function getFilters() {
+    let response = await fetch("http://localhost:5678/api/categories");
+    // let responseJson = await response.json();
+    // console.log(responseJson);
+    return await response.json();
+}
+
+async function displayFilters() {
+    let listFilters = await getFilters(); // Récupération response.json
+    let nav = document.createElement("nav"); // Création nav dans le DOM
+    let ul = document.createElement("ul"); // Création ul dans le DOM
+    filters.appendChild(nav); // Insérer la nav dans la class en tant qu'enfant
+    nav.appendChild(ul); // Insérer le ul dans la nav en tant qu'enfant
+    let all = document.createElement("li");
+    all.textContent = "Tous";
+    ul.appendChild(all);
+    // Boucle des noms de catégories
+    listFilters.forEach((filtersNav) => {
+        let li = document.createElement("li"); //  Création de la balise li
+        li.textContent = filtersNav.name; // Insérer le contenue de l'API (name)
+        ul.appendChild(li); // Insérer les li dans le ul en tant qu'enfant
+    });
+}
+
+// async function displayFilters() {
+//     let listFilters = await getFilters();
+//     listFilters.forEach((filtersNav) => {
+//         let nav = document.createElement("nav");
+//         let ul = document.createElement("ul");
+//         let li = document.createElement("li");
+//         filters.appendChild(nav);
+//         nav.appendChild(ul);
+//         ul.appendChild(li);
+//         li.textContent = filtersNav.name;
+//     });
+// }
+
+// Appel des function
+getFilters();
+displayFilters();
